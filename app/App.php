6800b4643066fbfdafbf65e1850a7877171cd4db
Yigit Agca <yigitagcame@gmail.com>
2022-02-06 21:51:40 +0200

    Command Line Service Added

--- app/App.php
+++ app/App.php
@@ -3,18 +3,23 @@
 namespace App;
 
 use App\Services\HttpService;
+use App\Services\CommandLineService;
 
 class App
 {
     private object $request;
+    private object $commandLine;
 
-    public function __construct(HttpService $apiRequest)
+    public function __construct(HttpService $apiRequest, CommandLineService $commandLine)
     {
         $this->request = $apiRequest;
+        $this->commandLine = $commandLine;
     }
 
     public function run() : void
     {
-        echo 'Heelo World!';
+        $this->commandLine->init(['c:'], ['city:']);
+        $city = $this->commandLine->input('city');
+        echo $city;
     }
 }
